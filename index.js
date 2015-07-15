728c211d1b12209b2fa85be583888bc1f5b16e9a
Samuel Castro <samuelcastrosilva@gmail.com>
2015-07-15 07:13:09 -0300

    Adding some comments

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
 
+/**
+ * @author Samuel Castro
+ * @description XML to JSON converter
+ * @since 7/14/15
+ * @type {xml2json}
+ */
 var xml2json = require('./lib/xml2json.js').run;
 
